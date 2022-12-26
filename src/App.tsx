ba1111875ca51760231f4d7217298fbaef8e0597
csabbah <csabbah@ryerson.ca>
2022-12-26 16:28:02 -0500

    Added init Cart setup (App.tsx). This includes adding Drawer (material-ui menu (can be closed on blur) to contain Cart component) & StyledButton (contains badge & opens cart) components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 // Styles
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
 // Types
 export type CartItemType = {
   id: number;
@@ -27,13 +27,18 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 function App() {
+  const [cartOpen, setCartOpen] = useState(false);
+  // cartItems will be an array of items (which is of CartItemType type)
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
   // ("products") is the query string we can name it whatever we want
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
 
-  const getTotalItems = () => null;
+  // Takes in all the data items
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -44,7 +49,16 @@ function App() {
   if (error) return <div>Something went wrong...</div>;
 
   return (
+    // Wrapper is an exported Styled component 'App.styles.ts', it acts as a regular .CSS file
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart Goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {!isLoading &&
           data?.map((item) => (
